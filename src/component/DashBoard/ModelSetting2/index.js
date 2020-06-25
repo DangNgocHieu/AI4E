0cc02e8b98b3957b0c9d1903986a4e6e0f2d5a21
Anh <anhduy0911@gmail.com>
2020-06-25 18:21:03 +0700

    API partly implemented

--- src/component/DashBoard/ModelSetting2/index.js
+++ src/component/DashBoard/ModelSetting2/index.js
@@ -1,112 +1,139 @@
-
-import React, { Component, useState} from 'react';
+import React, { Component, useState } from 'react';
 import { Collapse, Button, CardBody, Card } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown, faArrowLeft, faChevronLeft, faChevronRight, faPlus } from '@fortawesome/free-solid-svg-icons';
 import './index.scss';
 import { Link } from 'react-router-dom';
 import '../../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { getTrainedModels, getDomainModel, cookieManager } from '../../../api handler/api_manager';
 
-function Example(model_name) {
-  
-    const [isOpen, setIsOpen] = useState(false);
-    var title_ = "Model name";
-    const toggle = () => setIsOpen(!isOpen);
-  
-    return (
-      <div id="test">
-        
-        <a href="#" class="list-group-item btn-dark-blue" onClick={toggle} style={{ marginBottom: '1rem' }}>Model name</a>
-        <Collapse isOpen={isOpen}>
-          <Card>
-            <CardBody>
-           
+function ModelList(props) {
+
+  const [isOpen, setIsOpen] = useState(false);
+  var title_ = "Model name";
+  const toggle = () => setIsOpen(!isOpen);
+
+  let input_fields = props.model.input_fields.map((field) => (
+    <a href='#'>{field}&nbsp;</a>
+  ))
+
+  let output_fields = props.model.output_fields.map((field) => (
+    <a href='#'>{field}&nbsp;</a>
+  ))
+
+  return (
+    <div id="test">
+
+      <a href="#" class="list-group-item btn-dark-blue" onClick={toggle} style={{ marginBottom: '1rem' }}>Model name</a>
+      <Collapse isOpen={isOpen}>
+        <Card>
+          <CardBody>
+
             <div className="info_model">
-                                        <div className="column">
-                                            <p>Data field you have: </p>
-                                            <a href="#">Filed 01,</a>
-                                            <a href="#"> Filed 02,</a>
-                                            <a href="#"> Filed 03</a>
-                                        </div>
-                                        <div className="column">
-                                            <p>Data field you want to predict: </p>
-                                            <a href="#">Filed 01,</a>
-                                            <a href="#"> Filed 02,</a>
-                                            <a href="#"> Filed 04</a>
-                                        </div>
-                                        <div className="column">
-                                            <p>Time range you want to predict: </p>
-                                            <a href="#">1000</a>
-                                        </div>
-                                    </div>
+              <div className="column">
+                <p>Data field you have: </p>
+                {input_fields}
+              </div>
+              <div className="column">
+                <p>Data field you want to predict: </p>
+                {output_fields}
+              </div>
+              <div className="column">
+                <p>Time range you want to predict: </p>
+                <a href="#">{props.model.interval_unit}</a>
+              </div>
+            </div>
             <div class="btn-group" role="group" aria-label="Basic example">
-  <button type="button" class="btn btn-test"><Link to="/test">Test</Link></button>
-  <button type="button" class="btn btn-train"><Link to="/domain">Continue training</Link></button>
-  
-</div>
-            
-            </CardBody>
-          </Card>
-        </Collapse>
+              <button type="button" class="btn btn-test"><Link to="/test">Test</Link></button>
+              <button type="button" class="btn btn-train"><Link to="/domain">Continue training</Link></button>
+
+            </div>
+
+          </CardBody>
+        </Card>
+      </Collapse>
+    </div>
+  );
+}
+
+class ModelSetting2 extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      train_model: [],
+      domain_model: []
+    }
+  }
+
+  async componentDidMount() {
+    let user_id = cookieManager.getCookie('user_id')
+    let domain_id = this.props.domain_id
+
+    let trained_model = await getTrainedModels(user_id, domain_id)
+    let domain_model = await getDomainModel(domain_id)
+    this.setState({ trained_model: trained_model, domain_model: domain_model }, alert('API called successfully!'))
+  }
+
+  render() {
+
+    let trained_model_template = null
+    let domain_model_template = null
+
+    if (this.state.train_model.length !== 0)
+      trained_model_template = this.state.train_model.map((model) => (
+        <ModelList model={model} />
+      ))
+    if (this.state.domain_model.length !== 0)
+      trained_model_template = this.state.domain_model.map((model) => (
+        <ModelList model={model} />
+      ))
+
+    return (
+      <div className="wrraper_contaier">
+        <div className="dashboardRight">
+          <div className="dashboardInfo">
+            <div className="back">
+              <FontAwesomeIcon icon={faArrowLeft} />
+              <Link to={"/dashboard"}>Back</Link>
+            </div>
+            <div className="info">
+              <p className="text-name">Nguyen Phi Le</p>
+              <img src="https://scontent.fhan2-3.fna.fbcdn.net/v/t1.15752-9/105482679_317488752589979_7247942203476025085_n.png?_nc_cat=107&_nc_sid=b96e70&_nc_ohc=SEpidIU1VuUAX-H_7h8&_nc_ht=scontent.fhan2-3.fna&oh=b51a82c2a4d047565a9fbf7ca2ef25b5&oe=5F17B4A6"></img>
+              <FontAwesomeIcon className="icon" icon={faChevronDown} />
+            </div>
+          </div>
+          <div className="dashboardModelSetting">
+            <p className="text_modelsetting">Model Setting</p>
+            <div id="model_setting">
+
+
+              <div class="row">
+
+                <div class="col-5">
+                  <button type="button" class="btn title-btn"><h2>Trained Models</h2></button>
+
+                  <div class="list-group">
+                    {trained_model_template}
+                  </div>
+
+                </div>
+                {/* <div class="col-1"></div> */}
+                <div class="col-5">
+                  <button class="btn title-btn"><h2>Recommended Models</h2></button>
+                  <div class="list-group group2">
+                    {domain_model_template}
+                  </div>
+
+                </div>
+              </div>
+
+
+            </div>
+
+          </div>
+        </div>
       </div>
     );
   }
-
-class ModelSetting2 extends Component {
-    render() {
-        return (
-            <div className="wrraper_contaier">
-                <div className="dashboardRight">
-                    <div className="dashboardInfo">
-                        <div className="back">
-                            <FontAwesomeIcon icon={faArrowLeft} />
-                        <Link to={"/dashboard"}>Back</Link>
-                        </div>
-                        <div className="info">
-                            <p className="text-name">Nguyen Phi Le</p>
-                            <img src="https://scontent.fhan2-3.fna.fbcdn.net/v/t1.15752-9/105482679_317488752589979_7247942203476025085_n.png?_nc_cat=107&_nc_sid=b96e70&_nc_ohc=SEpidIU1VuUAX-H_7h8&_nc_ht=scontent.fhan2-3.fna&oh=b51a82c2a4d047565a9fbf7ca2ef25b5&oe=5F17B4A6"></img>
-                            <FontAwesomeIcon className="icon" icon={faChevronDown} />
-                        </div>
-                    </div>
-                    <div className="dashboardModelSetting">
-                        <p className="text_modelsetting">Model Setting</p>
-                        <div id="model_setting">
-                            
-                            
-                        <div class="row">
-    
-    <div class="col-5">
-      <button type = "button" class="btn title-btn"><h2>Trained Models</h2></button>
-      
-      <div class="list-group">
-        <Example/>
-        <Example/>
-        <Example/>
-    </div>
-    
-    </div>
-    {/* <div class="col-1"></div> */}
-    <div class="col-5">
-    <button class="btn title-btn"><h2>Recommended Models</h2></button>
-      <div class="list-group group2">
-        <Example/>
-        <Example/>
-        <Example/>
-      {/* <a href="#" class="list-group-item btn-ultra-voilet">First item</a>
-      <a href="#" class="list-group-item btn-ultra-voilet">Second item</a>
-      <a href="#" class="list-group-item btn-ultra-voilet">Third item</a> */}
-    </div>
-    
-    </div>
-    </div>
-    
-                           
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        );
-    }
 }
 export default ModelSetting2;
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,30 +1,24 @@
-import React  from 'react';
-export default function Login() {  
-  return (
-    <div id="login">         
-     <img className="img_banner" src="https://scontent.fhan2-4.fna.fbcdn.net/v/t1.15752-9/105361584_2330657780575465_8715217683182923322_n.png?_nc_cat=105&_nc_sid=b96e70&_nc_ohc=devGENwmnl8AX_sc73b&_nc_ht=scontent.fhan2-4.fna&oh=0460284f0d99659ebe209b60a32112ba&oe=5F15689E"></img>
+import React, { Component } from 'react';
+import LoginForm from '../../component/LoginForm'
+
+class Login extends Component {
+  render() {
+
+    return (<div id="login">
+      <img className="img_banner" src="https://scontent.fhan2-4.fna.fbcdn.net/v/t1.15752-9/105361584_2330657780575465_8715217683182923322_n.png?_nc_cat=105&_nc_sid=b96e70&_nc_ohc=devGENwmnl8AX_sc73b&_nc_ht=scontent.fhan2-4.fna&oh=0460284f0d99659ebe209b60a32112ba&oe=5F15689E"></img>
       <div className="loginbox">
-         <img class="img_title" src="https://scontent.fhan2-1.fna.fbcdn.net/v/t1.15752-9/105519883_329165518076048_2628760172393083726_n.png?_nc_cat=102&_nc_sid=b96e70&_nc_ohc=XW2PLHXg3R0AX-hKmiv&_nc_ht=scontent.fhan2-1.fna&oh=742d75d060287a3b127c0fdb20127b76&oe=5F17DCEB"></img>
+        <img class="img_title" src="https://scontent.fhan2-1.fna.fbcdn.net/v/t1.15752-9/105519883_329165518076048_2628760172393083726_n.png?_nc_cat=102&_nc_sid=b96e70&_nc_ohc=XW2PLHXg3R0AX-hKmiv&_nc_ht=scontent.fhan2-1.fna&oh=742d75d060287a3b127c0fdb20127b76&oe=5F17DCEB"></img>
         <section className="formlogin">
-          <div className="username">
-            <p className="text">UserName</p>
-            <input data-val="true" data-val-required="Nhập tên đăng nhập!" id="UserName" name="UserName" placeholder="Tên đăng nhập" tabindex="1" type="text" />
-          </div>
-          <div className="password">
-            <p className="text">Password</p>
-            <input data-val="true" data-val-required="Nhập mật khẩu!" id="Password" name="Password" placeholder="Nhập mật khẩu" tabindex="2" type="password" />
-          </div>
-          <div className="login">
-          <button class="btn_login" type="button"> Login </button>
-          </div>
+          <LoginForm />
         </section>
       </div>
       <div className="other">
         <span className="support">@AI4E 2020. All right reserved.</span>
       </div>
-      
-    </div>
-  );
+
+    </div>)
+
+  }
 }
 
-// export default Login;
+export default Login;
