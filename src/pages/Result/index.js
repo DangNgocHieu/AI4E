8c95fc9728876ae465096717e08d7f02c0320140
Dung Nguyen <thuydungnguyen.ictk63@gmail.com>
2020-06-28 03:01:35 +0700

    duy drp trai ccc

--- src/pages/Result/index.js
+++ src/pages/Result/index.js
@@ -11,37 +11,36 @@ import './index.scss';
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import User from '../User';
 import Chart from '../../img/chart1.png';
-import {Doughnut} from "react-chartjs-2";
+import {Line} from "react-chartjs-2";
 
-const label_fake = [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050];
+const label_fake = ["1/1", "2/1", "3/1", "4/1", "5/1", "6/1", "7/1", "8/1", "9/1", "10/1"];
 const train_fake = [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478];
+const test_fake = [168, 170, 178, 190, 203, 276, 408, 547, 675, 734];
+const y_label = "Predicted " + "field";
 class Progress extends Component {
 render() 
 {
     return(
         <div id="progress">
-            <div className="row">
+            <div className="row text-center">
                 <div className="col-3">
 <Menu2 />
                 </div>
                 <div className="col-8">
 
-    <div class="row text-center">
+    <div class="row">
     <UserInfo/>
-    <div class="col text-center">
+    </div>
+    <div class="row text-center">
     <h2>Predicting Result</h2>
+    </div>
     <div class="show-progress">
     <div className="row show-progress">
         
-        <div className="col-12">
-            <h3>This is the result page.</h3>
-        </div>
-  
-    
-    </div>
-    <div class="row illus text-center">
-        <p class="charshow">
+        <div class="row illus text-center">
+        <div class="charshow">
         {/* <img src={Chart} /> */}
+        <article className="canvas-container">
         <Line 
         data = {{
             labels: label_fake,
@@ -53,32 +52,47 @@ render()
           fill: false
                 },
 {
-                data: train_fake,
-                label: "Train",
-                borderColor: "#3e95cd",
+                data: test_fake,
+                label: "Groundtruth",
+                borderColor: "#8e5ea2",
       fill: false
             },
             ]
         }}
-        </p>
-       
-    </div>
-
-    <div class="row mt-5">
-        <div class="col-12 text-center">
-<button class="btn">Save Model</button>
-
+        options={{
+            title: {
+              display: true,
+              text: "Predicted "+ "field" + " by model " + "model_name",
+            },
+            legend: {
+              display: true,
+              position: "bottom"
+            },
+            responsive: true, maintainAspectRatio: true,
+          }} />
+          </article>
         </div>
        
     </div>
+    <div class="row save-btn mt-5">
+        <div class="col text-center">
+        <button class="btn">Save Model</button>
+        </div>
+   
+        </div>
+  
+    
     </div>
+    
+</div>
+    
+    
     
     </div>
 </div>
 
-                </div>
-            </div>
-        </div>
+      </div>         
+        
     )
 }
 }
